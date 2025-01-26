fff873d7a632e61bd665c88ba545b7edbb2521ac
nagendrayakkaladevara <nagendrayakkaladevara@gmail.com>
2025-01-26 21:58:42 +0530

    depoyment corrections

--- components/ColorPicker/colorpicker.tsx
+++ components/ColorPicker/colorpicker.tsx
@@ -33,7 +33,8 @@ const ColorPicker: React.FC = () => {
         const max = Math.max(rNorm, gNorm, bNorm);
         const min = Math.min(rNorm, gNorm, bNorm);
 
-        let h = 0, s = 0, l = (max + min) / 2;
+        let h = 0, s = 0;
+        const l = (max + min) / 2;
 
         if (max !== min) {
             const d = max - min;
